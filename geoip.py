d65ae45e47409c17e2b38a0f48ebeee5e6e8ae44
tmfkdkrp <tmfkd.krp@gmail.com>
2021-09-19 17:53:44 +0900

    geolite2 test

---
+++ geoip.py
@@ -0,0 +1,19 @@
+# this tools provides geolocation using maxmind geolite2 service
+import sys
+import geoip2.database
+import pandas as pd
+
+iplist = pd.read_csv("iplist.txt",header=None)
+#ipaddress=sys.argv[1]
+# periodically update the GeoLite2-City ip, specify the exact directory
+reader = geoip2.database.Reader('GeoLite2-City_20210914/GeoLite2-City.mmdb')
+
+for index, record in iplist.iterrows():
+	
+	if "#" in record[0]:
+		continue
+	else:
+		response = reader.city(record[0])
+		
+		#print(response.country)
+		print(record[0],response.country.names["en"],response.country.iso_code,sep=",")
